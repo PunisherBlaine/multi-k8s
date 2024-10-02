c727c145aa0a579bf3ef54cd5ec9bc98d6b67205
PunisherBlaine <blaine.jirip@gmail.com>
2024-10-02 13:27:35 +0800

    small change

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <h1>Fibonacci Calculator</h1>
+          <h1>Fibonacci Calculator v2 - K8S</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
